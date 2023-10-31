d897c6c16c3df8629c7bc07985c5220a612318a2
Benjamin Perron <98972450+SuperTardig@users.noreply.github.com>
2023-10-31 16:06:20 -0400

    Update mini_serv.c

--- mini_serv.c
+++ mini_serv.c
@@ -4,82 +4,82 @@
 #include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void sendAll(int id, int max, int *clients, char *msg){
-	for (int i = 0; i < max; i++)
-		if (clients[i] != id)
-			send(clients[i], msg, strlen(msg), SO_NOSIGPIPE);
-}
+int sockfd;
+struct sockaddr_in servaddr = {0};
+int max;
+int ids[FD_SETSIZE];
+int maxId = 0;
+fd_set active, ready;
+char msg[100] = {};
+char sending[2] = {};
 
-int getId(int id, int max, int *clients){
-	for (int i = 0; i < max; i++)
-		if (clients[i] == id)
-			return i;
-	return -1;
-}
-
-void writeError(char *msg){
-	write(2, msg, strlen(msg));
+void writeError(char* message){
+	write(2, message, strlen(message));
 	exit(1);
 }
 
-int main(int argc, char **argv) {
+void sendAll(int clientFd, char* message){
+	for (int fd = 0; fd < max + 1; fd++)
+		if (fd != clientFd)
+			send(fd, message, strlen(message), SO_NOSIGPIPE);
+}
+
+int main(int argc, char** argv) {
 	if (argc != 2)
 		writeError("Wrong number of arguments\n");
-
-	int maxId = 0, clients[FD_SETSIZE], sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	fd_set active, ready;
-	char buffer[200000];
-	if (sockfd < 0)
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1)
 		writeError("Fatal error\n");
-
-	struct sockaddr_in servaddr = {0}; 
 	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(atoi(argv[1])); 
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) < 0) 
-		writeError("Fatal error\n");
-	if (listen(sockfd, SOMAXCONN) < 0) 
+	servaddr.sin_addr.s_addr = htonl(2130706433);
+	servaddr.sin_port = htons(atoi(argv[1]));
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0 || listen(sockfd, SOMAXCONN) != 0)
 		writeError("Fatal error\n");
 
 	FD_ZERO(&active);
 	FD_SET(sockfd, &active);
-	int max = sockfd;
+	max = sockfd;
 
 	while (1){
 		ready = active;
-		if (select(max + 1, &ready, NULL, NULL, NULL) < 0)
-			writeError("Fatal error\n");
-		for (int id = 0; id <= max; id++){
-			if (FD_ISSET(id, &ready)){
-				if (id == sockfd){
-					int client = accept(sockfd, NULL, NULL);
-					if (client < 0)
-						continue;
-					max = client > max ? client : max;
-					FD_SET(client, &active);
-					clients[maxId] = client;
-					sprintf(buffer, "server: client %d just arrived\n", maxId++);
-					sendAll(client, maxId, clients, buffer); 
-				} else {
-					int read = recv(id, buffer, sizeof(buffer), 0);
-					if (read <= 0){
-						FD_CLR(id, &active);
-						close(id);
-						sprintf(buffer, "server: client %d just left\n", getId(id, maxId, clients));
-						clients[getId(id, maxId, clients)] = 0;
-						sendAll(id, maxId, clients, buffer);
-					} else {
-						char temp[200000];
-						buffer[read] = '\0';
-						if (buffer[read - 1] != '\n')
-							sprintf(temp, "client %d: %s\n", getId(id, maxId, clients), buffer);
-						else
-							sprintf(temp, "client %d: %s", getId(id, maxId, clients), buffer);
-						sendAll(id, maxId, clients, temp);
+		if (select(max + 1, &ready, NULL, NULL, NULL) < 0) 
+			continue;
+		for (int fd = 0; fd < max + 1; fd++){
+			if (!(FD_ISSET(fd, &ready)))
+				continue;
+			if (fd == sockfd){
+				int clientFd = accept(sockfd, NULL, NULL);
+				if (clientFd < 0)
+					continue;
+				max = clientFd > max ? clientFd : max;
+				ids[clientFd] = maxId++;
+				FD_SET(clientFd, &active);
+				sprintf(msg, "server: client %d just arrived\n", ids[clientFd]);
+				sendAll(clientFd, msg);
+			} else {
+				char clientBuf[200000];
+				memset(clientBuf, 0, sizeof(clientBuf));
+				int recvLen = recv(fd, clientBuf, 200000, 0);
+
+				if (recvLen > 0){
+					sprintf(msg, "client %d: ", ids[fd]);
+					sendAll(fd, msg);
+					for (int i = 0; i < recvLen; i++){
+						sending[0] = clientBuf[i];
+						sendAll(fd, sending);
+						if (i + 1 < recvLen && sending[0] == '\n')
+							sendAll(fd, msg);
 					}
+					if (sending[0] != '\n')
+						sendAll(fd, "\n");
+				} else {
+					sprintf(msg, "server: client %d just left\n", ids[fd]);
+					sendAll(fd, msg);
+					FD_CLR(fd, &active);
+					close(fd);
 				}
 			}
 		}
