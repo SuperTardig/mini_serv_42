635f6e0de60a89e556496952f8130a7c8528206e
Benjamin Perron <98972450+SuperTardig@users.noreply.github.com>
2023-10-31 07:43:20 -0400

    Update mini_serv.c

--- mini_serv.c
+++ mini_serv.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
@@ -8,92 +7,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void writeError(char *msg){
-	write(2, msg, strlen(msg));
-	exit(1);
-}
-
-void send_all(char *msg, int clients[1000], int id, int max){
-	for (int i = 0; i < max; i++)
+void sendAll(int id, int max, int *clients, char *msg){
+	for (int i = 0; i <= max; i++)
 		if (clients[i] != id)
 			send(clients[i], msg, strlen(msg), SO_NOSIGPIPE);
 }
 
-int getId(int id, int maxId, int *clients){
-	for (int i = 0; i < maxId; i++)
+int getId(int id, int max, int *clients){
+	for (int i = 0; i <= max; i++)
 		if (clients[i] == id)
 			return i;
 	return -1;
 }
 
+void write_error(char *msg){
+	write(2, msg, strlen(msg));
+	exit(1);
+}
+
+
 int main(int argc, char **argv) {
-
 	if (argc != 2)
-		writeError("Wrong number of arguments\n");
+		write_error("Wrong number of arguments\n");
 
-	int clients[FD_SETSIZE], id = 0, server = socket(AF_INET, SOCK_STREAM, 0);
+	int clients[FD_SETSIZE], id = 0, sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 	fd_set active, ready;
 	char buffer[200000];
 
-	if (server== -1)
-		writeError("Fatal Error\n");
+	if (sockfd < 0)
+		write_error("Fatal error\n");
 
-	struct sockaddr_in addres = {0};
+	struct sockaddr_in servaddr = {0};
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr.sin_port = htons(atoi(argv[1])); 
+  
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) < 0)
+		write_error("Fatal error\n");
 
-	addres.sin_family = AF_INET; 
-	addres.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-	addres.sin_port = htons(atoi(argv[1])); 
+	if (listen(sockfd, SOMAXCONN) < 0)
+		write_error("Fatal error\n");
 
-	if ((bind(server, (const struct sockaddr *)&addres, sizeof(addres))) < 0)
-		writeError("Fatal Error\n");
-
-	if (listen(server, SOMAXCONN) < 0)
-		writeError("Fatal Error\n");
-
+	int max = sockfd;
 	FD_ZERO(&active);
-	FD_SET(server, &active);
-	int max = server;
+	FD_SET(sockfd, &active);
 
-	while(1) {
+	while (1) {
 		ready = active;
 		if (select(max + 1, &ready, NULL, NULL, NULL) < 0)
-				writeError("Fatal Error\n");
+			write_error("Fatal error\n");
 
-		for (int socketId = 0; socketId <= max; socketId++){
-			if (FD_ISSET(socketId, &ready)){
-				printf("allo %d\n", socketId);
-				if (socketId == server){
-					int client = accept(server, NULL, NULL);
-					if (client > 0){
-						FD_SET(client, &active);
-						max = client > max ? client : max;
-						sprintf(buffer, "server: client %d just arrived\n", id);
-						send_all(buffer, clients, socketId, id);
-						clients[id++] = client;
-					}
-				} else{
-					int read = recv(socketId, buffer, sizeof(buffer), 0);
+		for (int i = 0; i <= max; i++){
+			if (FD_ISSET(i, &ready)){
+				if (i == sockfd){
+					int client = accept(sockfd, NULL, NULL);
+					if (client < 0)
+						continue;
+					FD_SET(client, &active);
+					max = client > max ? client : max;
+					clients[id] = client;
+					sprintf(buffer, "server: client %d just arrived\n", id++);
+					sendAll(client, id, clients, buffer);
+				} else {
+					int read = recv(i, buffer, sizeof(buffer), 0);
 
 					if (read <= 0){
-						sprintf(buffer, "server: client %d just left\n", getId(socketId, id, clients));
-						send_all(buffer, clients, socketId, id);
-						close(socketId);
-						clients[getId(socketId, id, clients)] = 0;
-						FD_CLR(socketId, &active);
-					}
-					else{
+						FD_CLR(i, &active);
+						close(i);
+						sprintf(buffer, "server: client %d just left\n", getId(i, id, clients));
+						clients[getId(i, id, clients)] = 0;
+						sendAll(i, id, clients, buffer);
+					} else {
 						char temp[200000];
 						buffer[read] = '\0';
 						if (buffer[read - 1] != '\n'){
 							buffer[read] = '\n';
 							buffer[read + 1] = '\0';
 						}
-						sprintf(temp, "client: %d: %s", getId(socketId, id, clients), buffer);
-						send_all(temp, clients, socketId, id);
+						sprintf(temp, "client %d: %s", getId(i, id, clients), buffer);
+						sendAll(i, id, clients, temp);
 					}
 				}
 			}
-		}
+		}		
 	}
 	return 0;
 }
